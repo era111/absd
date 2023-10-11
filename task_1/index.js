6285f76b50802a219398eec586c382e834e9ecb8
era111 <dmitrieva.era@yandex.ru>
2023-10-11 22:18:11 +0300

    my commit

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,21 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js"
 
 console.log("Let's rock")
 console.log(encoded, translations)
+function Decode() {
+  let uni = new Set()
+  const decoded = encoded.map((el) => {
+    let result = {}
+    for (let [key, value] of Object.entries(el)) {
+      uni.add(el.groupId)
+      if (!["groupId", "service", "formatSize", "ca"].includes(key))
+        result[key] = translations[value]
+      else result[key] = value === null ? null : value
+    }
+    return result
+  })
+  console.log("Уникальные: ", uni)
+  return decoded
+}
 
-
-
-// console.log(decoded)
+console.log(Decode())
--- task_2/src/lib/Counter.svelte
+++ task_2/src/lib/Counter.svelte
@@ -1,10 +1,49 @@
-<script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
+<script>
+  let data = {};
+  let selectedF = 'USD'
+  let selectedS = 'AED'
+  let answerF= 0;
+  let answerS= 0;
+	const fet = (async () => {
+		const response = await fetch('https://v6.exchangerate-api.com/v6/8939c45d21c9c66085fa91c8/latest/USD')
+    let prom = await response.json()
+    data = prom.conversion_rates
+    return data
+	})()
+
+  function handleFirstCHange(e) {
+    answerS  = e.target.value * data[selectedS] / data[selectedF]
   }
+
+  function handleSecondCHange(e) {
+    answerF  = e.target.value * data[selectedF] / data[selectedS]
+  }
+
+  function handleFselect() {
+    answerS  =answerF* data[selectedS] / data[selectedF]
+  }
+    function handleSselect() {
+    answerF  = answerS * data[selectedF] / data[selectedS]
+  }
+
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+	<select id="f_sel" bind:value={selectedF} on:change={handleFselect}>
+		{#each Object.entries(data) as [valute, count]}
+			<option value={valute}>
+				{valute}
+			</option>
+		{/each}
+	</select>
+
+<input value={answerF}  id="f_input" on:input={handleFirstCHange}/>
+
+ 	<select id="s_sel" bind:value={selectedS} on:change={handleSselect}>
+		{#each Object.entries(data) as [valute, count]}
+			<option value={valute}>
+				{valute}
+			</option>
+		{/each}
+	</select>
+
+<input id="s_input" value={answerS} on:input={handleSecondCHange}/>
